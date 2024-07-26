69a133a216c5f077cbb1a1b082c96755ba8bed8e
siznorias <siznorias@gmail.com>
2024-07-26 17:27:43 +0900

    - 단순 오타 수정 및 정리.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import {useState} from "react";
 
-function Board({ xIsNext, squares, onPlay }) {
-
-    // 승자 결정 TODO: squares 크기에 상관 없이 동작하도록 변경
-    function calculateWinner(squares) {
-        //한 변의 길이
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                console.log('??')
-                return squares[a];
-            }
+// 승자 계산 TODO: squares 크기에 상관 없이 동작하도록 변경
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
         }
-        return null;
     }
+    return null;
+}
+//보드판
+function Board({ xIsNext, squares, onPlay }) {
+    // 렌더링 시점에 승자 결정
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
@@ -32,9 +31,8 @@ function Board({ xIsNext, squares, onPlay }) {
         status = "Player Now: " + (xIsNext ? "O" : "X");
     }
 
-    // 클릭시
+    // 클릭시 onPlay로 배열 전달
     function handleClick(i) {
-
         const newSquares = squares.slice();
         if(!!newSquares[i] || calculateWinner(squares)){
             return;
@@ -60,25 +58,26 @@ function Board({ xIsNext, squares, onPlay }) {
     };
     return (<>
         <div className="status">{status}</div>
-        {renderRow(1)}
-        {renderRow(4)}
-        {renderRow(7)}
+        {renderRow(0)}
+        {renderRow(3)}
+        {renderRow(6)}
     </>);
 }
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
     const currentSquares = history[history.length - 1];
 
-    // 히스토리 저장 및 플레이어 변경
+    // 클릭시 플레이어 변경 및 히스토리 추가
     function handlePlay(nextSquares) {
         setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        setCurrentMove(currentMove - 1);
     }
     // 히스토리로 이동
     function jumpTo(nextStep) {
         setHistory(history.slice(0, nextStep + 1));
-        setXIsNext(nextStep % 2 === 0);
+        setCurrentMove(currentMove);
     }
     // move에 대한 렌더링
     const moves = history.map((squares, step) => {
