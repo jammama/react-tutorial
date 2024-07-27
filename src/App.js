8e0bf2b1649c84fb66f28688e3ca18ff19264b60
siznorias <siznorias@gmail.com>
2024-07-27 23:42:58 +0900

    - board 사이즈 변경 가능하도록

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function calculateWinner(squares) {
 }
 
 //보드판
-function Board({xIsNext, squares, onPlay}) {
+function Board({xIsNext, squares, onPlay, row}) {
     // 렌더링 시점에 승자 결정
     const winner = calculateWinner(squares);
     let status;
@@ -46,36 +46,37 @@ function Board({xIsNext, squares, onPlay}) {
         onPlay(newSquares);
     }
 
-    // JSP 수정이 귀찮아서 렌더링 함수 추가
+    // square 렌더링
     const renderSquare = (i) => {
         return <Square value={squares[i]} onSquareClick={() => handleClick(i)}/>;
     };
-    const renderRow = (i) => {
-        return (<div className="board-row">
-            {renderSquare(i)}
-            {renderSquare(i + 1)}
-            {renderSquare(i + 2)}
-        </div>);
-    };
-    return (<>
-        <div className="status">{status}</div>
-
-        {renderRow(0)}
-        {renderRow(3)}
-        {renderRow(6)}
-    </>);
+    // 한 줄 렌더링
+    const renderRow = (row, i) =>
+        <div className="board-row">
+            {Array.from({length: row}, (x, y) => y + row * i)
+                .map((r, j) => renderSquare(r))}
+        </div>;
+    // 가로세로 렌더링
+    const renderBox = (row) => Array.from({length: row})
+        .map((r, i) => renderRow(row, i))
+    return (
+        <>
+            <div className="status">{status}</div>
+            {renderBox(row)}
+        </>
+    );
 }
 
 export default function Game() {
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [currentMove, setCurrentMove] = useState(0);
+    const [currentMove, setCurrentMove] = useState(-1);
     const xIsNext = currentMove % 2 === 0;
     const currentSquares = history[history.length - 1];
 
     // 클릭시 플레이어 변경 및 히스토리 추가
     function handlePlay(nextSquares) {
         setHistory([...history, nextSquares]);
-        setCurrentMove(history.length - 1 );
+        setCurrentMove(history.length - 1);
     }
 
     // 히스토리로 이동
@@ -85,29 +86,40 @@ export default function Game() {
     }
 
     // move에 대한 렌더링
+    const button = (step) => step === currentMove + 1 ? <label>Now : {step}</label> :
+        <button onClick={() => jumpTo(step)}>Go to move #{step}</button>
     const moves = history.map((squares, step) => {
-        return (
-            <>
-                <li key={step}>
-                    { step === currentMove + 1 ? <label>Now : {step}</label> :
-                        <button onClick={() => jumpTo(step)}>Go to move #{step} {currentMove}</button>}
-                </li>
-            </>
-        );
-    });
+            return (
+                <>
+                    <li key={step}>
+                        {button(step)}
+                    </li>
+                </>
+            )
+                ;
+        })
+    ;
+    const [row, setRow] = useState(3);
     return (
-        <div className="game">
-            <div className="game-board">
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+        <>
+            <div className="game">
+                <div className="game-board">
+                    <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} row={row}/>
+                </div>
+                <div className="game-info">
+                    <button onClick={() => jumpTo(0)}>restart</button>
+                    <ol>{moves}</ol>
+                </div>
             </div>
-            <div className="game-info">
-                <button onClick={() => jumpTo(0)}>restart</button>
-                <ol>{moves}</ol>
-            </div>
-        </div>
+            <label for="row"> board size : {row} </label>
+            <br/>
+            <input id="row" value={row} onInput={(e) => setRow(e.target.value)} type="range" max="9"/>
+        </>
     );
 }
 
-function Square({value, onSquareClick}) {
+function Square({
+                    value, onSquareClick
+                }) {
     return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
